b6141e495ae4998320c03b9d216b8a4b7cecbf13
adrianodsv <tsiadriano@gmail.com>
2022-09-09 08:25:57 -0300

    Add comments

--- API/controllers/getTakeRepo.js
+++ API/controllers/getTakeRepo.js
@@ -3,12 +3,17 @@ const axios = require('axios')
 module.exports = {
     async index(request, response){
         try{
+            //Requisição na API do Github
             let {data} = await axios ('https://api.github.com/orgs/takenet/repos')
+
+            //Filtrar array para exibir somente repositórios com a linguagem C#
             data = data.filter(function(item) {
                 return item.language === "C#"
             });
 
             let repos = []
+
+            //Montar array com os 5 primeiros itens do array data
             for (let index = 0; index < 5; index++) {
                 repos.push({
                     name: data[index].name,

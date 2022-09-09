0ad17207e548169a30360ad3be2d81dd06553863
adrianodsv <tsiadriano@gmail.com>
2022-09-08 22:12:01 -0300

    Change response structure to add status and avatar_image

--- API/controllers/getTakeRepo.js
+++ API/controllers/getTakeRepo.js
@@ -8,16 +8,23 @@ module.exports = {
                 return item.language === "C#"
             });
 
-            let res = []
+            let repos = []
             for (let index = 0; index < 5; index++) {
-                res.push({
+                repos.push({
                     name: data[index].name,
                     description: data[index].description,
                     created_at: data[index].created_at
                 });
             }
+
+            let res = {
+                status: "success",
+                avatar_image: data[0].owner.avatar_url,
+                repos: repos
+            }
             return response.json(res)
         }catch(e){
+            console.log(e)
             response.status(500).send('Something broke! Sorry for this :/');
         }
         
